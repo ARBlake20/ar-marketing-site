4651f2aee99a93c27f4fa57fcde1fe20cf53440d
Blake Martin <blake@a-rmarketing.com>
2025-04-08 14:30:25 -0400

    Fix layout and AOS init

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import "@/styles/globals.css";
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+import { useEffect } from 'react';
+import '@/styles/globals.css';
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    AOS.init({
+      once: false,
+      duration: 1200,
+      easing: 'ease-out-cubic',
+      offset: 100,
+    });
+  }, []);
+
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -146,16 +146,3 @@ export default function Home() {
     </main>
   );
 }
-import 'aos/dist/aos.css';
-import AOS from 'aos';
-import { useEffect } from 'react';
-
-function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    AOS.init({ once: true });
-  }, []);
-
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
